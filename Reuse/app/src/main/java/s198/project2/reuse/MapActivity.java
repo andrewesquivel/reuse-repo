730ba3307feaa3abb4cd3fcef6dfbbad4d386ce0
Drew <abe707@mit.edu>
2015-04-22 02:07:44 -0400

    added google maps api xml file

--- Reuse/app/src/main/java/s198/project2/reuse/MapActivity.java
+++ Reuse/app/src/main/java/s198/project2/reuse/MapActivity.java
@@ -5,12 +5,32 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android
+
 
 public class MapActivity extends ActionBarActivity {
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+
+        if (map!=null){
+            Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+                    .title("Hamburg"));
+            Marker kiel = map.addMarker(new MarkerOptions()
+                    .position(KIEL)
+                    .title("Kiel")
+                    .snippet("Kiel is cool")
+                    .icon(BitmapDescriptorFactory
+                            .fromResource(R.drawable.ic_launcher)));
+        }
+
         setContentView(R.layout.activity_map);
     }
 
