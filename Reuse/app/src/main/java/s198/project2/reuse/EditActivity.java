31bca5c027a4378d0e8afd576b689fcf8511d9e7
Rachel Wang <rswang@mit.edu>
2015-05-12 23:02:06 -0400

    readd edit menu

--- Reuse/app/src/main/java/s198/project2/reuse/EditActivity.java
+++ Reuse/app/src/main/java/s198/project2/reuse/EditActivity.java
@@ -59,7 +59,7 @@ public class EditActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_item, menu);
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
         return true;
     }
 
